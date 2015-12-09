e47abfa616faf6d8679ea6122e961775d113ef04
pikej <jpike21@gmail.com>
2015-12-09 14:49:39 -0500

    adding new changes

--- src/com/soapexample/RunTest.java
+++ src/com/soapexample/RunTest.java
@@ -8,7 +8,8 @@ public class RunTest {
 	public static void main(String args[]) throws FileNotFoundException {
 
 		Weather weather = new Weather();
-		String xml = weather.getWeather();
+		String xml = weather.getWeather("90210");
+		System.out.println(xml);
 		System.out.println(weather.isSuccess(xml));
 	}
 
--- src/com/soapexample/Weather.java
+++ src/com/soapexample/Weather.java
@@ -6,16 +6,17 @@ import com.jayway.restassured.path.xml.XmlPath;
 
 public class Weather {
 	
-	final static String WEATHER_ENVELOPE= "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:weat=\"http://ws.cdyne.com/WeatherWS/\"><soapenv:Header/><soapenv:Body><weat:GetCityForecastByZIP><!--Optional:--><weat:ZIP>90210</weat:ZIP></weat:GetCityForecastByZIP></soapenv:Body></soapenv:Envelope>";
+	final static String WEATHER_ENVELOPE= "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:weat=\"http://ws.cdyne.com/WeatherWS/\"><soapenv:Header/><soapenv:Body><weat:GetCityForecastByZIP><!--Optional:--><weat:ZIP>YYY</weat:ZIP></weat:GetCityForecastByZIP></soapenv:Body></soapenv:Envelope>";
 	
 	
-	public String getWeather(){
+	public String getWeather(final String zipCode){
 		
+		String weather = WEATHER_ENVELOPE.replace("YYY", zipCode);
 		return given()
 				.request()
 				.header("SOAPAction",
 						"http://ws.cdyne.com/WeatherWS/GetCityForecastByZIP")
-				.contentType("text/xml;charset=UTF-8").body(WEATHER_ENVELOPE).expect()
+				.contentType("text/xml;charset=UTF-8").body(weather).expect()
 				.statusCode(200).when()
 				.post("http://wsf.cdyne.com/WeatherWS/Weather.asmx").asString();
 		
